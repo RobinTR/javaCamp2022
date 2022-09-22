96f3c0e611d1698bda16557d0681f0d085b7db50
Mem Robin Çen <memrobincen@gmail.com>
2022-09-22 16:45:37 +0300

    Day 2 homework 2 done

---
+++ src/week2/homeworks/inheritance/src/inheritance/PersonManager.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class PersonManager {
+	public void list() {
+		System.out.println("Müşteri listelendi.");
+	}
+
+	public void add() {
+		System.out.println("Müşteri eklendi.");
+	}
+
+}
